14be29a67afe1c7e0dac6488dc709d86c2b04697
TheBenefactour <81839696+TheBenefactour@users.noreply.github.com>
2021-04-07 23:44:36 -0400

    Update app.py
    Added tag exclusions to search

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with open('user_data.json', 'r', encoding='utf-8') as f:
 LIVE = True
 INDEX_URL = 'https://raw.githubusercontent.com/TheBenefactour/emcsabrowser/main/indexed_stories.json'
 ROOT = 'http://localhost:5000/'
-TAGS_LIST = '<p><fieldset name=tags><h1>Tags:</h1>' \
+TAGS_LIST_ADD = '<p><fieldset name=tags><h2>Include Tags:</h2>' \
             '<p><input type="checkbox" name="md" value="md "/><label for="md">Male Dominant</label>' \
             '<input type="checkbox" name="fd" value="fd" /><label for="fd">Female Dominant</label>' \
             '<input type="checkbox" name="ca" value="ca" /><label for="ca">Cannibalism</label></p><p>' \
@@ -39,8 +39,38 @@ TAGS_LIST = '<p><fieldset name=tags><h1>Tags:</h1>' \
             '<input type="checkbox" name="ts" value="ts" /><label for="ts">Time Stop</label>' \
             '<input type="checkbox" name="ws" value="ws" /><label for="ws">Watersports</label>' \
             '</p></fieldset></p>'
+TAGS_LIST_REM = '<p><fieldset name=tags-rem><h2>Exclude Tags:</h2>' \
+            '<p><input type="checkbox" name="md-rem" value="md-rem"/><label for="md-rem">Male Dominant</label>' \
+            '<input type="checkbox" name="fd-rem" value="fd-rem" /><label for="fd-rem">Female Dominant</label>' \
+            '<input type="checkbox" name="ca-rem" value="ca-rem" /><label for="ca-rem">Cannibalism</label></p><p>' \
+            '<input type="checkbox" name="cb-rem" value="cb-rem" />' \
+            '<label for="cb-rem">Comic Book: Super-hero/heroine</label>' \
+            '<input type="checkbox" name="ds-rem" value="ds-rem" />' \
+            '<label for="ds-rem">Dominance and/or Submission</label>' \
+            '<input type="checkbox" name="ff-rem" value="ff-rem" />' \
+            '<label for="ff-rem">Female/Female Sex</label></p><p>' \
+            '<input type="checkbox" name="ft-rem" value="ft-rem" /><label for="ft-rem">Fetish</label>' \
+            '<input type="checkbox" name="fu-rem" value="fu-rem" /><label for="fu-rem">Furry</label>' \
+            '<input type="checkbox" name="gr-rem" value="gr-rem" />' \
+            '<label for="gr-rem">Growth/Enlargement of Bodies and Parts</label>' \
+            '</p><p><input type="checkbox" name="hm-rem" value="hm-rem" /><label for="hm-rem">Humiliation</label>' \
+            '<input type="checkbox" name="hu-rem" value="hu-rem" /><label for="hu-rem">Humor</label>' \
+            '<input type="checkbox" name="in-rem" value="in-rem" /><label for="in-rem">Incest</label></p><p>' \
+            '<input type="checkbox" name="la-rem" value="la-rem" /><label for="la-rem">Lactation</label>' \
+            '<input type="checkbox" name="ma-rem" value="ma-rem" /><label for="ma-rem">Masturbation</label>' \
+            '<input type="checkbox" name="mc-rem" value="mc-rem" /><label for="mc-rem">Mind Control</label></p><p>' \
+            '<input type="checkbox" name="mf-rem" value="mf-rem" /><label for="mf-rem">Male/Female Sex</label>' \
+            '<input type="checkbox" name="mm-rem" value="mm-rem" /><label for="ex-rem">Male/Male Sex</label>' \
+            '<input type="checkbox" name="nc-rem" value="nc-rem" /><label for="nc-rem">Non-consensual</label></p><p>' \
+            '<input type="checkbox" name="rb-rem" value="rb-rem" /><label for="ex-rem">Robots</label>' \
+            '<input type="checkbox" name="sc-rem" value="sc-rem" /><label for="sc-rem">Scatology</label>' \
+            '<input type="checkbox" name="sf-rem" value="sf-rem" /><label for="sf-rem">Science Fiction</label></p><p>' \
+            '<input type="checkbox" name="ts-rem" value="ts-rem" /><label for="ts-rem">Time Stop</label>' \
+            '<input type="checkbox" name="ws-rem" value="ws-rem" /><label for="ws-rem">Watersports</label>' \
+            '</p></fieldset></p>'
 TAGS = ['bd', 'be', 'ca', 'cb', 'ds', 'ex', 'fd', 'ff', 'ft', 'fu', 'gr', 'hm', 'hu', 'in', 'la', 'ma', 'mc', 'md',
         'mf', 'mm', 'nc', 'rb', 'sc', 'sf', 'ts', 'ws']
+TAGS_REM = [f'{i}-rem' for i in TAGS]
 
 app = Flask(__name__)
 webbrowser.open(ROOT)
@@ -59,8 +89,8 @@ if LIVE:
 
 
 def update_user_data():
-    with open('user_data.json', 'w', encoding='utf-8') as f:
-        json.dump(USER_DATA, f)
+    with open('user_data.json', 'w', encoding='utf-8') as g:
+        json.dump(USER_DATA, g)
 
 
 update_user_data()
@@ -69,7 +99,7 @@ with open('indexed_stories.json', 'r', encoding='utf-8') as f:
 
 
 @app.route('/')
-def root():
+def app_root():
     return f'<span><p><a href=/list>List</a></p><p><a href=/random>Random</a></p>' \
            f'<p><a href=/search>Search</a></p><p><a href=/favorites>Favorites</a></p></span>'
 
@@ -124,9 +154,9 @@ def list_files():
                 iterate(d)
 
             # exclude/include files
-            files = [os.path.join(root, f) for f in files]
-            files = [f for f in files if not re.match(excludes, f)]
-            files = [f for f in files if re.match(includes, f)]
+            files = [os.path.join(root, h) for h in files]
+            files = [h for h in files if not re.match(excludes, h)]
+            files = [h for h in files if re.match(includes, h)]
 
             for filename in files:
                 file_list.append(filename)
@@ -134,9 +164,9 @@ def list_files():
     iterate(location)
 
     out_str = '<span>'
-    for f in file_list:
+    for g in file_list:
         out_str += '<p>'
-        out_str += f.split('\\')[-1]
+        out_str += g.split('\\')[-1]
         out_str += '</p>'
     out_str += '</span>'
     return out_str
@@ -158,20 +188,17 @@ def search():
     try:
         term = request.form['query']
         tags = []
+        tags_rem = []
         for i in request.form:
-            if i != 'query':
+            if i in TAGS:
                 tags.append(i)
-        temp_list = []
-        if len(tags) == 0:
-            tags = 'all'
-            temp_list = STORY_DATA
-        else:
-            for i in STORY_DATA:
-                if set(STORY_DATA[i]['story tags']).intersection(tags):
-                    temp_list.append(i)
+            elif i in TAGS_REM:
+                tags_rem.append(i)
+        tags, temp_list = filter_story_list_by_tags(tags, tags_rem)
         out_list = f'<div><form method="post"><input type="text" name="query">' \
                    f'<input type="submit" label="Submit"></form></div>' \
-                   f'<div><p>Searching for: "{term}" with {tags} tags</p></div><form method="post">' \
+                   f'<div><p>Searching for: "{term}" including {tags} tags and excluding {[i[0:2] for i in tags_rem]}' \
+                   f'</p></div><form method="post">' \
                    f'<div style="position:fixed; background-color:white; width:100%"><p>' \
                    f'<input type="submit" value="Add selected to favorites."></p></div>' \
                    f'<div style="padding-top: 50px">'
@@ -180,20 +207,35 @@ def search():
                 story_added = False
                 for t in term.split(' '):
                     if t in i or t in STORY_DATA[i]['description'] and not story_added:
-                        out_list += f'<div><input type="checkbox" id="{i}" name="{i}" value="{i}">' + \
+                        out_list += f'<div><input type="checkbox" id="{i}" name="{i}" value="{i}">' \
+                                    f'<label for="{i}">Add to favorites</label>' + \
                                     get_story_string(i) + '</div><hr class="dotted">'
                         story_added = True
             except KeyError:
                 pass
         if out_list:
             return out_list + '</form>'
-    except Exception as e:
+    except Exception:
         out_list = []
         for i in request.form:
             out_list.append(i)
         add_favorites(out_list)
         return f'<form method="post"><p><input type="text" name="query" /><input type="submit" label="Submit" /></p>' \
-               f'{TAGS_LIST}</div></form>'
+               f'<fieldset><h1>Tags</h1>{TAGS_LIST_ADD}{TAGS_LIST_REM}</div></form>'
+
+
+def filter_story_list_by_tags(tags, tags_rem):
+    temp_list = []
+    if len(tags) == 0:
+        tags = 'all'
+        temp_list = STORY_DATA
+    else:
+        for i in STORY_DATA:
+            story_tags_set = set(STORY_DATA[i]['story tags'])
+            if story_tags_set.intersection(tags) != set() and \
+                    set([f'{i}-rem' for i in story_tags_set]).intersection(tags_rem) == set():
+                temp_list.append(i)
+    return tags, temp_list
 
 
 @app.route('/favorites')
